32fd41586c583141f4526723eacf5d489fab8e93
wang-yuandong <yuandong.wang@qq.com>
2024-03-29 11:16:59 +0800

    修复路径问题

--- src/utils/create.js
+++ src/utils/create.js
@@ -1,6 +1,10 @@
 import fs from 'fs'
 import ejs from 'ejs'
 import path from 'path'
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const fileHeader = `
 /* eslint-disable */
